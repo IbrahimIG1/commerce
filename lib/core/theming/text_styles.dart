56675c2f61894168c563281f4224103d53b41575
Ibrahim Algammal <ibrahim13elgammal@gmail.com>
2024-08-08 14:37:01 +0300

    - added color manager class - added font_weight_helper class - added text_styles class

---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:g_commerce/core/theming/font_weight.dart';
+
+class TextStyles {
+  // Black
+  final TextStyle font18BoldBlack = TextStyle(
+    color: Colors.black,
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+}
