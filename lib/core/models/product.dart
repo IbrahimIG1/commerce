97e75549b3b8e040ba641b8bf1c68019d8ee8de1
Ibrahim Algammal <ibrahim13elgammal@gmail.com>
2024-08-18 16:44:09 +0300

    - added person model - added admin model extends from person  model - added user model extends from person  model

--- lib/core/models/product.dart
+++ lib/core/models/product.dart
@@ -3,37 +3,33 @@ class Product {
   final String name; // book - shirt
   final String price; // 100 EGP
   final String discount; // 10%
-  final String productOwner; // algammal
   final String productType; // clothes - school tools
   final String productPicutre; // clothes - school tools
   Product({
     required this.name,
     required this.price,
     required this.discount,
-    required this.productOwner,
     required this.productType,
     required this.productPicutre,
   });
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'name': name,
       'product_picutre': productPicutre,
       'price': price,
       'discount': discount,
-      'product_owner': productOwner,
       'product_type': productType,
     };
   }
 
-  factory Product.fromMap(Map<String, dynamic> map) {
+  factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      name: map['name'] as String,
-      productPicutre: map['product_picutre'] as String,
-      price: map['price'] as String,
-      discount: map['discount'] as String,
-      productOwner: map['product_owner'] as String,
-      productType: map['product_type'] as String,
+      name: json['name'] as String,
+      productPicutre: json['product_picutre'] as String,
+      price: json['price'] as String,
+      discount: json['discount'] as String,
+      productType: json['product_type'] as String,
     );
   }
 
